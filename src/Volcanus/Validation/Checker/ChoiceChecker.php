efb23895f7af1b39ac5dd802c48c20cc5307dc9f
k-holy <k_horii@rikcorp.co.jp>
2012-05-07 14:34:42 +0900

    add choice checker

---
+++ src/Volcanus/Validation/Checker/ChoiceChecker.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * PHP versions 5
+ *
+ * @copyright  2011 k-holy <k.holy74@gmail.com>
+ * @author     k.holy74@gmail.com
+ * @license    http://www.opensource.org/licenses/mit-license.php  The MIT License (MIT)
+ */
+namespace Volcanus\Validation\Checker;
+
+use Volcanus\Validation\Util;
+use Volcanus\Validation\Exception\InvalidArgumentException;
+use Volcanus\Validation\Exception\CheckerException\ChoiceException;
+
+/**
+ * ChoiceChecker
+ *
+ * @author     k.holy74@gmail.com
+ */
+class ChoiceChecker extends AbstractChecker
+{
+
+	public function __construct(array $options = array())
+	{
+		$this->options['choices'] = null; // 選択肢 (Array/Traversable または カンマ区切りの文字列)
+		$this->options = Util::mergeOptions($this->options, $options);
+	}
+
+	/**
+	 * 値が指定された配列の要素に含まれる値と同一かどうかを検証します。
+	 *
+	 * @param  mixed   検証値 (文字列または__toStringメソッド実装オブジェクト)
+	 * @param  array   検証オプション
+	 * @return boolean 検証結果
+	 */
+	public function check($value, array $options = array())
+	{
+		$options = Util::mergeOptions($this->options, $options);
+
+		$choices  = $options['choices'];
+
+		if (!isset($choices)) {
+			throw new InvalidArgumentException(
+				'The parameter "choices" is not specified.');
+		}
+		if (is_string($choices) && false !== strpos($choices, ',')) {
+			$choices = explode(',', $choices);
+		}
+		if (!is_array($choices) && !($choices instanceof Traversable)) {
+			throw new InvalidArgumentException(
+				'The parameter "choices" is not valid Array/Traversable.');
+		}
+
+		$stringValue = (string)$value;
+
+		$found = false;
+		foreach ($choices as $choice) {
+			if ($choice == $stringValue) {
+				$found = true;
+				break;
+			}
+		}
+		if (!$found) {
+			throw new ChoiceException(
+				'The value your choice is not valid.');
+		}
+
+		return true;
+	}
+
+}
