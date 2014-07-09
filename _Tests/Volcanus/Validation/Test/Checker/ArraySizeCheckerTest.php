49d5282067ea73a84c14bd55ce6df5bf65b11ee6
k-holy <k.holy74@gmail.com>
2014-07-09 20:14:41 +0900

    src に移動、Tests→_Testsに変更

---
+++ _Tests/Volcanus/Validation/Test/Checker/ArraySizeCheckerTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * PHP versions 5
+ *
+ * @copyright  2011 k-holy <k.holy74@gmail.com>
+ * @author     k.holy74@gmail.com
+ * @license    http://www.opensource.org/licenses/mit-license.php  The MIT License (MIT)
+ */
+namespace Volcanus\Validation\Test\Checker;
+
+use Volcanus\Validation\Checker\ArraySizeChecker;
+
+/**
+ * ArraySizeCheckerTest
+ *
+ * @author     k.holy74@gmail.com
+ */
+class ArraySizeCheckerTest extends \PHPUnit_Framework_TestCase
+{
+
+	protected $checker;
+
+	public function setUp()
+	{
+		$this->checker = new ArraySizeChecker();
+	}
+
+	public function testCheckIsOk()
+	{
+		$this->assertTrue($this->checker->check(array(1,2,3), array('minSize' => 1)));
+		$this->assertTrue($this->checker->check(array(1,2,3), array('minSize' => 1, 'maxSize' => 3)));
+		$this->assertTrue($this->checker->check(array(1,2,3), array('minSize' => 3, 'maxSize' => 3)));
+	}
+
+	/**
+	 * @expectedException Volcanus\Validation\Exception\CheckerException\MinValueException
+	 */
+	public function testRaiseMinValueExceptionWhenCheckIsNgByMinValue()
+	{
+		$this->checker->check(array(1,2,3), array('minSize' => 4));
+	}
+
+	/**
+	 * @expectedException Volcanus\Validation\Exception\CheckerException\MaxValueException
+	 */
+	public function testRaiseMaxValueExceptionWhenCheckIsNgByMaxValue()
+	{
+		$this->checker->check(array(1,2,3), array('minSize' => 1, 'maxSize' => 2));
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseInvalidArgumentExceptionWhenValueIsNotArrayAndNotTraversable()
+	{
+		$this->checker->check('123');
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseInvalidArgumentExceptionWhenInvalidMinValueParameterIsSpecified()
+	{
+		$this->checker->check(array(1,2,3), array('minSize' => 'A', 'maxSize' => 4));
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseInvalidArgumentExceptionWhenInvalidMaxValueParameterIsSpecified()
+	{
+		$this->checker->check(array(1,2,3), array('minSize' => 1, 'maxSize' => 'A'));
+	}
+
+	/**
+	 * @expectedException Volcanus\Validation\Exception\CheckerException\MinValueException
+	 */
+	public function testInvokeMethod()
+	{
+		$checker = $this->checker;
+		$checker->setOption('minSize', 4);
+		$checker(array(1,2,3));
+	}
+
+}
---
+++ _Tests/Volcanus/Validation/Test/Checker/UriCheckerTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * PHP versions 5
+ *
+ * @copyright  2011 k-holy <k.holy74@gmail.com>
+ * @author     k.holy74@gmail.com
+ * @license    http://www.opensource.org/licenses/mit-license.php  The MIT License (MIT)
+ */
+namespace Volcanus\Validation\Test\Checker;
+
+use Volcanus\Validation\Checker\UriChecker;
+
+/**
+ * UriCheckerTest
+ *
+ * @author     k.holy74@gmail.com
+ */
+class UriCheckerTest extends \PHPUnit_Framework_TestCase
+{
+
+	protected $checker;
+
+	public function setUp()
+	{
+		$this->checker = new UriChecker();
+	}
+
+	public function testCheckIsOk()
+	{
+		$this->assertTrue($this->checker->check('http://www.example.com/'));
+	}
+
+	public function testCheckIsOkWithAcceptSchemes()
+	{
+		$this->assertTrue($this->checker->check('https://www.example.com/', array('acceptScheme' => 'https')));
+		$this->assertTrue($this->checker->check('http://userinfo@reg-name:8080/path/to/file.ext?foo=bar#1'));
+		$this->assertTrue($this->checker->check('http://www.example.com/'));
+		$this->assertTrue($this->checker->check('http://日本語ドメイン.com/'));
+	}
+
+	/**
+	 * @expectedException Volcanus\Validation\Exception\CheckerException\UriException
+	 */
+	public function testRaiseUriExceptionWhenCheckIsNgByFormat()
+	{
+		$this->checker->check('::');
+	}
+
+	/**
+	 * @expectedException Volcanus\Validation\Exception\CheckerException\UriException
+	 */
+	public function testRaiseUriExceptionWhenCheckIsNgBySchemeIsNotAccepted()
+	{
+		$this->checker->check('ftp://www.example.com/', array('acceptScheme' => 'http,https'));
+	}
+
+	/**
+	 * @expectedException Volcanus\Validation\Exception\CheckerException\UriException
+	 */
+	public function testInvokeMethod()
+	{
+		$checker = $this->checker;
+		$checker->setOption('acceptScheme', 'http,https');
+		$checker('ftp://www.example.com/');
+	}
+
+}
